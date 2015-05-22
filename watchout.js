ab658c07279b62549ecb2f8d12f7888f63db30e5
tchan247 <tchan247@gmail.com>
2015-05-22 15:00:10 -0700

    Make asteroid svg

--- watchout.js
+++ watchout.js
@@ -1,2 +1,11 @@
 // start slingin' some d3 here.
+// var asteroid =
+d3.select(".main").append("svg")
+  .attr("height", 200)
+  .attr("width", 200)
+  // .attr("r", 10)
+  .style("background-image", "url('asteroid.png')");
+
+
+
 

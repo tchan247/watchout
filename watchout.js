963790aaa988d3a09a122e5e4c168744611db06a
tchan247 <tchan247@gmail.com>
2015-05-22 15:53:09 -0700

    Make asteroids scatter randomly

--- watchout.js
+++ watchout.js
@@ -1,11 +1,27 @@
 // start slingin' some d3 here.
 // var asteroid =
-d3.select(".main").append("svg")
-  .attr("height", 200)
-  .attr("width", 200)
-  // .attr("r", 10)
-  .style("background-image", "url('asteroid.png')");
+var asteroidCount = 10;
+var createAsteroid = function() {
+  var x = Math.random() * 800;
+  var y = Math.random() * 800;
+  d3.select(".mainSvg").append("circle")
+       .attr("cy", y)
+       .attr("cx", x)
+       .attr("r", 50)
+       .attr("fill", "red")
 
+    // .attr("height", 200)
+    // .attr("width", 200)
+    // .attr("y", y)
+    // .attr("x", x)
+    // .attr
+    // // .attr("position", "absolute")
+    // // .style("background-image", "url('asteroid.png')")
+    //  .transform("translate(50, 50)");
 
+}
 
-
+while(asteroidCount > 0) {
+  createAsteroid();
+  asteroidCount--;
+}

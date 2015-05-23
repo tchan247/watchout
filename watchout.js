433d351fbe20fbc2c5fb31074b987e93ffba12e9
tchan247 <tchan247@gmail.com>
2015-05-22 17:13:25 -0700

    Finish asteroid transition and data binding

--- watchout.js
+++ watchout.js
@@ -1,13 +1,14 @@
 // start slingin' some d3 here.
 // var asteroid =
-var asteroidCount = 10;
+var asteroidCount = 20;
 var createAsteroid = function() {
   var x = Math.random() * 800;
   var y = Math.random() * 800;
+  var asteroidSize = Math.random() * 50;
   d3.select(".mainSvg").append("circle")
        .attr("cy", y)
        .attr("cx", x)
-       .attr("r", 50)
+       .attr("r", asteroidSize)
        .attr("fill", "red")
 
     // .attr("height", 200)
@@ -18,10 +19,33 @@ var createAsteroid = function() {
     // // .attr("position", "absolute")
     // // .style("background-image", "url('asteroid.png')")
     //  .transform("translate(50, 50)");
-
 }
-
 while(asteroidCount > 0) {
   createAsteroid();
   asteroidCount--;
 }
+
+// var update = function(){
+
+  //   .attr();
+// };
+// d3.select(".mainSvg").selectAll("circle").transition().duration(2000).attr("fill", "blue");
+var moveAsteroid = function(){
+  var main = document.getElementsByClassName("mainSvg");
+  var children = main[0].childNodes;
+  var data = [];
+
+  for(var i = 0; i < children.length; i++) {
+    var newX = Math.random() * 800;
+    var newY = Math.random() * 800;
+    data.push({x: newX, y: newY});
+  }
+
+  d3.select(".mainSvg").selectAll("circle").data(data).transition().duration(1000).attr("cx", function(d){return d.x})
+  .attr("cy", function(d){return d.y});
+
+
+
+};
+
+setInterval(moveAsteroid, 1000);
